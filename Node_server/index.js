0c9cd05808c2d33f6b0e46533d7595babf6f071a
yogeshtakeo <yogesh@takeo.ai>
2023-04-09 17:38:59 -0400

    Added AWS JSON file

--- Node_server/index.js
+++ Node_server/index.js
@@ -6,9 +6,29 @@ app.get("/", (req, res) => {
   for (let i = 1; i < 10000000; i++) {
     console.log("Server :" + i);
   }
-  res.send("<h1>Started the server</h1>");
+  res.json("<h1>Started the server</h1>");
 });
 
+const pets = [
+  {
+    id: 1,
+    type: "dog",
+    price: 249.99,
+  },
+  {
+    id: 2,
+    type: "cat",
+    price: 124.99,
+  },
+  {
+    id: 3,
+    type: "fish",
+    price: 0.99,
+  },
+];
+app.get("/pets", (req, res) => {
+  res.send(pets);
+});
 app.listen(3000, () => {
   console.log("Server is being hosted in the 3000 port.");
 });
